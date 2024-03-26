23bae367dac9248385acc3ba60dafa4600b5c2b2
liangmiQwQ <mi_yyds@126.com>
2024-03-26 21:11:18 +0800

    feat: get death type

--- src/main/java/net/mirolls/bettertips/mixin/BetterTipsDied.java
+++ src/main/java/net/mirolls/bettertips/mixin/BetterTipsDied.java
@@ -1,32 +1,31 @@
 package net.mirolls.bettertips.mixin;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.entity.damage.DamageType;
 import net.minecraft.text.Text;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(DamageSource.class)
 public abstract class BetterTipsDied {
+    @Shadow @Final @Nullable private Entity source;
 
-    @Inject(method = "onDeath", at = @At("RETURN"), cancellable = true)
-    private void onGetDeathMessage(DamageSource damageSource, CallbackInfo info) {
-        if ((Object) this instanceof PlayerEntity player) {
+    @Inject(method="getDeathMessage", at= @At("RETURN"))
+    private void onDeath(LivingEntity killed, CallbackInfoReturnable<Text> cir){
+        System.out.printf("[BetterTips]%s was killed",killed.getName().getString());
 
-            // 取消默认的死亡消息
-            info.cancel();
+        // 处理死亡原因
+        DamageSource source = (DamageSource)(Object)this; // 将当前Mixin实例转换为DamageSource
+        DamageType deathType = source.getType(); // 使用msgId字段获取死亡类型的ID
 
-            // 构造自定义的死亡消息
-            Text customDeathMessage = Text.literal(player.getName().getString() + " 在这里遇到了不幸，原因是：" + damageSource.getDeathMessage(player).getString());
 
-            // 发送消息到所有玩家
-            ServerWorld world = (ServerWorld) player.getWorld();
-            for (PlayerEntity p : world.getPlayers()) {
-                p.sendMessage(customDeathMessage, false);
-            }
-        }
+//        killed.onDeath();
     }
 }
