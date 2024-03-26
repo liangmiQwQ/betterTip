f929edfdd60f4a2651f07a6a70f8c74a035cf2a1
liangmiQwQ <mi_yyds@126.com>
2024-03-26 20:39:48 +0800

    feat: add died info

--- src/main/java/net/mirolls/bettertips/mixin/BetterTipsDied.java
+++ src/main/java/net/mirolls/bettertips/mixin/BetterTipsDied.java
@@ -2,18 +2,31 @@ package net.mirolls.bettertips.mixin;
 
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class BetterTipsDied {
 
     @Inject(method = "onDeath", at = @At("RETURN"), cancellable = true)
-    private void onGetDeathMessage(DamageSource source, CallbackInfoReturnable<Text> cir) {
-        Text customDeathMessage = Text.literal("这里是自定义的死亡消息");
-        cir.setReturnValue(customDeathMessage);
+    private void onGetDeathMessage(DamageSource damageSource, CallbackInfo info) {
+        if ((Object) this instanceof PlayerEntity player) {
+
+            // 取消默认的死亡消息
+            info.cancel();
+
+            // 构造自定义的死亡消息
+            Text customDeathMessage = Text.literal(player.getName().getString() + " 在这里遇到了不幸，原因是：" + damageSource.getDeathMessage(player).getString());
+
+            // 发送消息到所有玩家
+            ServerWorld world = (ServerWorld) player.getWorld();
+            for (PlayerEntity p : world.getPlayers()) {
+                p.sendMessage(customDeathMessage, false);
+            }
+        }
     }
 }
