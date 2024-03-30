e15328a327a25fd512df5e02cab07fba194cd52f
liangmiQwQ <mi_yyds@126.com>
2024-03-30 10:47:06 +0800

    feat: add more and more mixin

--- src/main/java/net/mirolls/bettertips/mixin/BetterTipsEntityDeath.java
+++ src/main/java/net/mirolls/bettertips/mixin/BetterTipsEntityDeath.java
@@ -5,18 +5,15 @@ import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static net.mirolls.bettertips.BetterTips.LOGGER;
 
 
 @Mixin(PlayerEntity.class)
 public abstract class BetterTipsEntityDeath {
-    @Inject(method = "onDeath", at = @At("RETURN"))
-    private void onDeath(DamageSource damageSource, CallbackInfo ci) {
-        // 为了检测
-        LOGGER.info("[BetterTips]" + "发现有人死亡" + damageSource);
-        LOGGER.info("[BetterTips]" + damageSource.getType());
+    @Inject(method = "handleFallDamage", at = @At("RETURN"))
+    private void handleFallDamage(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+        LOGGER.info("[BetterTips]受到摔落伤害");
     }
-
 }
