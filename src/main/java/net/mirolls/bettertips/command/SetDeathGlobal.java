56483c7ba605be25bf955b14742f34eb3da82092
liangmiQwQ <mi_yyds@126.com>
2024-04-04 14:06:55 +0800

    feat: return an error if unknown id

--- src/main/java/net/mirolls/bettertips/command/SetDeathGlobal.java
+++ src/main/java/net/mirolls/bettertips/command/SetDeathGlobal.java
@@ -1,8 +1,11 @@
 package net.mirolls.bettertips.command;
 
-import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.text.Text;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
@@ -12,14 +15,29 @@ public class SetDeathGlobal {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("setDeathGlobal")
                 .requires(source -> source.hasPermissionLevel(2))
                 .then(literal("set")
-                        .then(argument("deathID", IntegerArgumentType.integer())
+                        .then(argument("deathID", StringArgumentType.string()) // 理论上来说，ID是String类型的
                                 .executes(context -> {
-                                    context.getSource().sendFeedback(() -> Text.literal("set模式"), false);
-                                    return 1;
+                                    String deathID = StringArgumentType.getString(context, "deathID");
+
+                                    // 先进行一个校验，防止整个配置文件崩坏
+                                    if (isValidDeathYamlKey(deathID)) {
+                                        context.getSource().sendFeedback(() -> Text.literal("set模式"), false);
+                                        return 1;
+                                    } else {
+                                        context.getSource().sendFeedback(() -> Text.translatable("command.unknown.argument"), false);
+                                        return 0;
+                                    }
                                 })))
                 .then(literal("query")
                         .executes(context -> {
                             return 1;
                         }))));
+    }
+
+    private static boolean isValidDeathYamlKey(String key) {
+        String regex = "^(death\\.attack\\.|death\\.fell\\.)[._a-zA-Z]*$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(key);
+        return matcher.matches();
     }
 }
