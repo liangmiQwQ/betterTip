cd9a9f56099abfd32b9e0d67433c6961b650e3f6
liangmiQwQ <mi_yyds@126.com>
2024-04-05 09:29:13 +0800

    refactor: use random to replace colorful

--- src/main/java/net/mirolls/bettertips/color/MinecraftColor.java
+++ src/main/java/net/mirolls/bettertips/color/MinecraftColor.java
@@ -16,7 +16,7 @@ public class MinecraftColor {
     public static MutableText getMinecraftTextWithColor(String text, String color) {
         MutableText basicText = Text.literal(text);
         if (color != null) { // 如果你是null，不好意思我们没有共同语言
-            if (!color.endsWith("colorful") && !color.startsWith("#")) { // 这个情况是如果不是colorful和start不是#的情况
+            if (!color.endsWith("random") && !color.startsWith("#")) { // 这个情况是如果不是random和start不是#的情况
                 // 尝试获取对应的Formatting枚举，如果颜色无效，则colorFormatting为null
                 Formatting colorFormatting = Formatting.byName(color.toUpperCase());
 
@@ -26,10 +26,10 @@ public class MinecraftColor {
                 }
                 // 如果颜色无效，可以在这里应用默认颜色或者不做任何操作
                 // 例如：basicText.setStyle(Style.EMPTY.withColor(Formatting.WHITE));
-            } else if (Objects.equals(color, "colorful")) {
-                basicText = createColorfulText(text);
-            } else if (Objects.equals(color, "light_colorful")) {
-                // 和createColorfulText差不多的
+            } else if (Objects.equals(color, "random")) {
+                basicText = createRandomColorText(text);
+            } else if (Objects.equals(color, "light_random")) {
+                // 和createRandomColorText差不多的
                 MutableText resultText = Text.empty(); // 创建一个空的MutableText作为起始点
                 for (char c : basicText.getString().toCharArray()) {
                     String RandomLightColor = generateBrightColor();
@@ -40,8 +40,8 @@ public class MinecraftColor {
                     resultText.append(charText); // 将字符文本附加到结果文本上
                 }
                 basicText = resultText; // 令basic等同添加完毕的
-            } else if (Objects.equals(color, "dark_colorful")) {
-                // 和createColorfulText差不多的
+            } else if (Objects.equals(color, "dark_random")) {
+                // 和createRandomColorText差不多的
                 MutableText resultText = Text.empty(); // 创建一个空的MutableText作为起始点
                 for (char c : basicText.getString().toCharArray()) {
                     TextColor colorHex = TextColor.fromRgb(hexToRgb(generateDarkColor())); // 创建一个颜色
@@ -51,8 +51,8 @@ public class MinecraftColor {
                     resultText.append(charText); // 将字符文本附加到结果文本上
                 }
                 basicText = resultText;
-            } else if (Objects.equals(color, "very_light_colorful")) {
-                // 和createColorfulText差不多的
+            } else if (Objects.equals(color, "very_light_random")) {
+                // 和createRandomColorText差不多的
                 MutableText resultText = Text.empty(); // 创建一个空的MutableText作为起始点
                 for (char c : basicText.getString().toCharArray()) {
                     String RandomLightColor = generateVeryBrightColor();
@@ -63,8 +63,8 @@ public class MinecraftColor {
                     resultText.append(charText); // 将字符文本附加到结果文本上
                 }
                 basicText = resultText; // 令basic等同添加完毕的
-            } else if (Objects.equals(color, "very_dark_colorful")) {
-                // 和createColorfulText差不多的
+            } else if (Objects.equals(color, "very_dark_random")) {
+                // 和createRandomColorText差不多的
                 MutableText resultText = Text.empty(); // 创建一个空的MutableText作为起始点
                 for (char c : basicText.getString().toCharArray()) {
                     TextColor colorHex = TextColor.fromRgb(hexToRgb(generateVeryDarkColor())); // 创建一个颜色
@@ -84,7 +84,7 @@ public class MinecraftColor {
         return basicText;
     }
 
-    public static MutableText createColorfulText(String message) {
+    public static MutableText createRandomColorText(String message) {
         MutableText resultText = Text.empty(); // 创建一个空的MutableText作为起始点
 
         for (char c : message.toCharArray()) {
